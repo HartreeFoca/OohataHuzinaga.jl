fb2444e29cf85ecf16e41ac6e63b13f208c87d6f
Leticia-maria <leticiapequeno30@gmail.com>
2023-10-29 20:32:24 -0400

    Remove DIIS

--- src/hartreefock.jl
+++ src/hartreefock.jl
@@ -23,7 +23,7 @@ function nuclearrepulsion(molecule::Molecule)
     return Vnn
 end
 
-function rhf(basis, molecule::Molecule, maxiter = 20, convergence = 1e-6, maxdiis = 10)
+function rhf(basis, molecule::Molecule, maxiter = 20, convergence = 1e-6)
     S = overlap(basis)
     println("Overlap is done!")
 
@@ -47,9 +47,6 @@ function rhf(basis, molecule::Molecule, maxiter = 20, convergence = 1e-6, maxdii
 
     Eel = 0.0
 
-    error_vectors = []
-    Fock_list = []
-
     println("Starting SCF iterations...")
 
     for iteration = 0:maxiter
@@ -63,33 +60,6 @@ function rhf(basis, molecule::Molecule, maxiter = 20, convergence = 1e-6, maxdii
 
         F = Hcore + P
         
-        error = F * D * S - S * D * F
-        push!(error_vectors, error)
-        push!(Fock_list, F)
-
-        if length(error_vectors) > maxdiis
-            popfirst!(error_vectors)
-            popfirst!(Fock_list)
-        end
-
-        if length(error_vectors) > 1
-            dim_B = length(error_vectors) + 1
-            B = zeros(dim_B, dim_B)
-            B[dim_B, dim_B] = 0.0
-
-            for i in 1:length(error_vectors), j in 1:length(error_vectors)
-                B[i, j] = sum(error_vectors[i] .* error_vectors[j])
-                B[dim_B, i] = -1.0
-                B[i, dim_B] = -1.0
-            end
-
-            rhs = zeros(dim_B)
-            rhs[dim_B] = -1.0
-            coeffs = B \ rhs
-
-            F = sum(coeffs[i] * Fock_list[i] for i in 1:length(error_vectors))
-        end
-
         Fp = X * F * X  
 
         eigen_decomp = eigen(Fp)   
