d0f096720585ebee1255a0dbdf70bd7063090a66
Leticia-maria <leticiapequeno30@gmail.com>
2024-04-16 09:56:12 -0400

    Remove tests that are not working

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,8 @@
+using BasisSets
 using OohataHuzinaga
 using Test
-using BasisSets
-
 @testset "OohataHuzinaga.jl" begin
-    include("doublefactorial.jl")
-    include("normalization.jl")
     include("overlap.jl")
     include("kinetic.jl")
     include("attraction.jl")
-    include("repulsion.jl")
 end
